70de00a474e7d001c0b8be3da2b706d10deb775c
yesheng <yec22@mails.tsinghua.edu.cn>
2022-12-11 15:51:29 +0800

    v2.0

--- model/utils.py
+++ model/utils.py
@@ -1,5 +1,6 @@
 import jittor as jt
 import numpy as np
+import imageio, tqdm, os
 
 img2mse = lambda x, y : jt.mean((x - y) ** 2)
 mse2psnr = lambda x : -10. * jt.log(x) / jt.log(jt.float32([10.]))
@@ -14,7 +15,6 @@ def img2psnr(x, y):
     psnr = jt.stack(psnrs).mean()
     return psnr
 
-
 def get_rays(H, W, focal, c2w):
     # sample point in 2D image space
     i, j = jt.meshgrid(jt.linspace(0, W-1, W), jt.linspace(0, H-1, H))
@@ -25,7 +25,6 @@ def get_rays(H, W, focal, c2w):
     rays_d = jt.sum(dirs[..., np.newaxis, :] * c2w[:3,:3], -1)
     rays_o = c2w[:3,-1].expand(rays_d.shape)
     return rays_o, rays_d
-
 
 # Hierarchical sampling, Reference: https://github.com/bmild/nerf
 def sample_pdf(bins, weights, N_samples, det=False):
@@ -87,7 +86,6 @@ def raw2outputs(raw, z_vals, rays_d):
 
     return rgb_map, disp_map, acc_map, weights, depth_map, extra_info
 
-
 def sample_test_ray(rays_o, rays_d, viewdirs, network, z_vals, network_query):
     pts = rays_o[...,None,:] + rays_d[...,None,:] * z_vals[...,:,None]
     raw = network_query(pts, viewdirs, network)
@@ -98,3 +96,165 @@ def sample_test_ray(rays_o, rays_d, viewdirs, network, z_vals, network_query):
     rgb_map, disp_map, acc_map, weights, depth_map, others = raw2outputs(raw, z_vals, rays_d)
 
     return rgb, sigma, depth_map
+
+def batchify(fn, chunk): 
+    def ret(inputs):
+        return jt.concat([fn(inputs[i:i+chunk]) for i in range(0, inputs.shape[0], chunk)], 0)
+    return ret
+
+# batchify the ray as input
+def batchify_rays(rays_flat, chunk=1024*32, **kwargs):
+    all_ret = {}
+    for i in range(0, rays_flat.shape[0], chunk):
+        ret = render_rays(rays_flat[i:i+chunk], **kwargs)
+        for k in ret:
+            if k not in all_ret:
+                all_ret[k] = []
+            all_ret[k].append(ret[k])
+    all_ret = {k : jt.concat(all_ret[k], 0) for k in all_ret}
+    return all_ret
+
+# volumetric rendering.
+def render_rays(ray_batch,
+                network_fn,
+                network_query_fn,
+                N_samples,
+                perturb=0.0,
+                N_importance=0,
+                network_fine=None,
+                sigma_loss=None,
+                entropy_ray_zvals=None,
+                ):
+    
+    N_rays = ray_batch.shape[0]
+    rays_o, rays_d = ray_batch[:,0:3], ray_batch[:,3:6] # [N_rays, 3]
+    viewdirs = ray_batch[:,-3:] if ray_batch.shape[-1] > 9 else None
+    bounds = jt.reshape(ray_batch[...,6:8], [-1,1,2])
+    near, far = bounds[...,0], bounds[...,1]
+
+    t_vals = jt.linspace(0., 1., steps=N_samples)
+    z_vals = near * (1.-t_vals) + far * (t_vals)
+    z_vals = z_vals.expand([N_rays, N_samples])
+
+    if perturb > 0.0:
+        mids = 0.5 * (z_vals[...,1:] + z_vals[...,:-1])
+        upper = jt.concat([mids, z_vals[...,-1:]], -1)
+        lower = jt.concat([z_vals[...,:1], mids], -1)
+        # random perturb
+        t_rand = jt.rand(z_vals.shape)
+        z_vals = lower + (upper - lower) * t_rand
+
+    pts = rays_o[...,None,:] + rays_d[...,None,:] * z_vals[...,:,None] # [N_rays, N_samples, 3]
+
+    if network_fn is not None:
+        raw = network_query_fn(pts, viewdirs, network_fn)
+        rgb_map, disp_map, acc_map, weights, depth_map, others = raw2outputs(raw, z_vals, rays_d)
+    else:
+        raw = network_query_fn(pts, viewdirs, network_fine)
+        rgb_map, disp_map, acc_map, weights, depth_map, others = raw2outputs(raw, z_vals, rays_d)
+
+    # hierarchical sampling
+    if N_importance > 0:
+        rgb_map_0, disp_map_0, acc_map_0 = rgb_map, disp_map, acc_map
+
+        z_vals_mid = 0.5 * (z_vals[...,1:] + z_vals[...,:-1])
+        z_samples = sample_pdf(z_vals_mid, weights[...,1:-1], N_importance, det=(perturb==0.0))
+        z_samples = z_samples.detach()
+
+        z_vals = jt.float32(np.sort(jt.concat([z_vals, z_samples], -1).numpy(), axis=-1))
+        pts = rays_o[...,None,:] + rays_d[...,None,:] * z_vals[...,:,None] # [N_rays, N_samples + N_importance, 3]
+
+        run_fn = network_fn if network_fine is None else network_fine
+        raw = network_query_fn(pts, viewdirs, run_fn)
+
+        rgb_map, disp_map, acc_map, weights, depth_map, others = raw2outputs(raw, z_vals, rays_d)
+
+    ret = {'rgb_map' : rgb_map, 'disp_map' : disp_map, 'acc_map' : acc_map, 'depth_map' : depth_map}
+    ret['sigma'] = others['sigma']
+    ret['alpha'] = others['alpha']
+    ret['z_vals'] = z_vals
+    ret['dists'] = others['dists'] 
+    ret['xyz'] = jt.sum(weights.unsqueeze(-1)*pts, -2)
+    ret['raw'] = raw
+
+    if N_importance > 0:
+        ret['rgb0'] = rgb_map_0
+        ret['disp0'] = disp_map_0
+        ret['acc0'] = acc_map_0
+
+    return ret
+
+# render function to get rgb_map, depth_map ...
+def render(H, W, focal, chunk=1024*32, rays=None, c2w=None, ndc=True,
+                  near=0., far=1., c2w_staticcam=None, depths=None,
+                  **kwargs):
+    if c2w is not None:
+        # render full image
+        rays_o, rays_d = get_rays(H, W, focal, c2w)
+    else:
+        # use provided ray batch
+        rays_o, rays_d = rays
+
+    viewdirs = rays_d
+    if c2w_staticcam is not None:
+        # visualize effect of viewdirs
+        rays_o, rays_d = get_rays(H, W, focal, c2w_staticcam)
+    viewdirs = viewdirs / jt.norm(viewdirs, dim=-1, keepdim=True)
+    viewdirs = jt.reshape(viewdirs, [-1,3]).float()
+
+    sh = rays_d.shape
+
+    # create ray batch
+    rays_o = jt.reshape(rays_o, [-1,3]).float()
+    rays_d = jt.reshape(rays_d, [-1,3]).float()
+
+    near, far = near * jt.ones_like(rays_d[...,:1]), far * jt.ones_like(rays_d[...,:1])
+    rays = jt.concat([rays_o, rays_d, near, far], -1) # B x 8
+    if depths is not None:
+        rays = jt.concat([rays, depths.reshape(-1,1)], -1)
+    rays = jt.concat([rays, viewdirs], -1)
+
+    # render and reshape
+    all_ret = batchify_rays(rays, chunk, **kwargs)
+    for k in all_ret:
+        k_sh = list(sh[:-1]) + list(all_ret[k].shape[1:])
+        all_ret[k] = jt.reshape(all_ret[k], k_sh)
+
+    k_extract = ['rgb_map', 'disp_map', 'acc_map', 'depth_map']
+    ret_list = [all_ret[k] for k in k_extract]
+    ret_dict = {k : all_ret[k] for k in all_ret if k not in k_extract}
+    return ret_list + [ret_dict]
+
+# render a sequence of images given by specific render poses (a render path)
+def render_path(render_poses, hwf, chunk, render_kwargs, gt_imgs=None, savedir=None):
+    H, W, focal = hwf
+    rgbs = []
+    disps = []
+    accs = []
+    
+    for i, c2w in enumerate(tqdm(render_poses)):
+        rgb, disp, acc, depth, extras = render(H, W, focal, chunk=chunk, c2w=c2w[:3,:4], **render_kwargs)
+        rgbs.append(rgb.numpy())
+        disps.append(disp.numpy())
+        accs.append(acc.numpy())
+        
+        if savedir is not None:
+            rgb8 = to8b(rgb.numpy())
+            rgb8[np.isnan(rgb8)] = 0
+            filename = os.path.join(savedir, '{:03d}.png'.format(i))
+            imageio.imwrite(filename, rgb8) # save rgb images
+            depth = depth.numpy()
+            depth = (np.clip(depth / 5, 0, 1) * 255.0).astype(np.uint8)
+            imageio.imwrite(os.path.join(savedir, '{:03d}_depth.png'.format(i)), depth) # save depth images
+        
+        # to save memory
+        del rgb
+        del disp
+        del acc
+        del extras
+        del depth
+
+    rgbs = np.stack(rgbs, 0)
+    disps = np.stack(disps, 0)
+
+    return rgbs, disps
--- train.py
+++ train.py
@@ -1,6 +1,5 @@
 # Reference: https://github.com/mjmjeong/InfoNeRF
 import os, sys, random
-import imageio
 import configargparse
 from tqdm import tqdm, trange
 import jittor as jt
@@ -19,97 +18,8 @@ np.random.seed(0)
 jt.set_global_seed(0)
 random.seed(0)
 
-def batchify(fn, chunk): 
-    def ret(inputs):
-        return jt.concat([fn(inputs[i:i+chunk]) for i in range(0, inputs.shape[0], chunk)], 0)
-    return ret
-
-# batchify the ray as input
-def batchify_rays(rays_flat, chunk=1024*32, **kwargs):
-    all_ret = {}
-    for i in range(0, rays_flat.shape[0], chunk):
-        ret = render_rays(rays_flat[i:i+chunk], **kwargs)
-        for k in ret:
-            if k not in all_ret:
-                all_ret[k] = []
-            all_ret[k].append(ret[k])
-
-    all_ret = {k : jt.concat(all_ret[k], 0) for k in all_ret}
-    return all_ret
-
-# volumetric rendering.
-def render_rays(ray_batch,
-                network_fn,
-                network_query_fn,
-                N_samples,
-                perturb=0.0,
-                N_importance=0,
-                network_fine=None,
-                sigma_loss=None,
-                entropy_ray_zvals=None,
-                ):
-    
-    N_rays = ray_batch.shape[0]
-    rays_o, rays_d = ray_batch[:,0:3], ray_batch[:,3:6] # [N_rays, 3]
-    viewdirs = ray_batch[:,-3:] if ray_batch.shape[-1] > 9 else None
-    bounds = jt.reshape(ray_batch[...,6:8], [-1,1,2])
-    near, far = bounds[...,0], bounds[...,1]
-
-    t_vals = jt.linspace(0., 1., steps=N_samples)
-    z_vals = near * (1.-t_vals) + far * (t_vals)
-    z_vals = z_vals.expand([N_rays, N_samples])
-
-    if perturb > 0.0:
-        mids = 0.5 * (z_vals[...,1:] + z_vals[...,:-1])
-        upper = jt.concat([mids, z_vals[...,-1:]], -1)
-        lower = jt.concat([z_vals[...,:1], mids], -1)
-        # random perturb
-        t_rand = jt.rand(z_vals.shape)
-        z_vals = lower + (upper - lower) * t_rand
-
-    pts = rays_o[...,None,:] + rays_d[...,None,:] * z_vals[...,:,None] # [N_rays, N_samples, 3]
-
-    if network_fn is not None:
-        raw = network_query_fn(pts, viewdirs, network_fn)
-        rgb_map, disp_map, acc_map, weights, depth_map, others = raw2outputs(raw, z_vals, rays_d)
-    else:
-        raw = network_query_fn(pts, viewdirs, network_fine)
-        rgb_map, disp_map, acc_map, weights, depth_map, others = raw2outputs(raw, z_vals, rays_d)
-
-    # hierarchical sampling
-    if N_importance > 0:
-        rgb_map_0, disp_map_0, acc_map_0 = rgb_map, disp_map, acc_map
-
-        z_vals_mid = 0.5 * (z_vals[...,1:] + z_vals[...,:-1])
-        z_samples = sample_pdf(z_vals_mid, weights[...,1:-1], N_importance, det=(perturb==0.0))
-        z_samples = z_samples.detach()
-
-        z_vals = jt.float32(np.sort(jt.concat([z_vals, z_samples], -1).numpy(), axis=-1))
-        pts = rays_o[...,None,:] + rays_d[...,None,:] * z_vals[...,:,None] # [N_rays, N_samples + N_importance, 3]
-
-        run_fn = network_fn if network_fine is None else network_fine
-        raw = network_query_fn(pts, viewdirs, run_fn)
-
-        rgb_map, disp_map, acc_map, weights, depth_map, others = raw2outputs(raw, z_vals, rays_d)
-
-    ret = {'rgb_map' : rgb_map, 'disp_map' : disp_map, 'acc_map' : acc_map, 'depth_map' : depth_map}
-    ret['sigma'] = others['sigma']
-    ret['alpha'] = others['alpha']
-    ret['z_vals'] = z_vals
-    ret['dists'] = others['dists'] 
-    ret['xyz'] = jt.sum(weights.unsqueeze(-1)*pts, -2)
-    ret['raw'] = raw
-
-    if N_importance > 0:
-        ret['rgb0'] = rgb_map_0
-        ret['disp0'] = disp_map_0
-        ret['acc0'] = acc_map_0
-
-    return ret
-
 # instantiate NeRF's MLP model. (coarse and fine)
 def create_nerf(args):
-    
     embed_fn, input_ch = get_embedder(10)
     embeddirs_fn, input_ch_views = get_embedder(4)
     skips = [4]
@@ -126,7 +36,6 @@ def create_nerf(args):
                                                                 embed_fn=embed_fn,
                                                                 embeddirs_fn=embeddirs_fn,
                                                                 netchunk=args.netchunk)
-
     # create optimizer
     optimizer = jt.nn.Adam(params=grad_vars, lr=args.lrate, betas=(0.9, 0.999))
 
@@ -143,7 +52,6 @@ def create_nerf(args):
             ckpt_path = os.path.join(os.path.join(basedir,expname, f'{args.ckpt_render_iter:06d}.tar'))
 
         ckpt = jt.load(ckpt_path)
-
         start = ckpt['global_step']
         optimizer.load_state_dict(ckpt['optimizer_state_dict'])
 
@@ -181,9 +89,7 @@ def run_network(inputs, viewdirs, fn, embed_fn, embeddirs_fn, netchunk=1024*64):
     outputs = jt.reshape(outputs_flat, list(inputs.shape[:-1]) + [outputs_flat.shape[-1]])
     return outputs
 
-
 def train(args):
-
     # load data
     images, poses, render_poses, hwf, i_split = load_data(args.datadir, args.testskip)
     i_train, i_val, i_test = i_split
@@ -305,7 +211,6 @@ def train(args):
             alpha_raw = extras['alpha']
 
         extras = {x:extras[x][:N_rgb] for x in extras}
-
         rgb = rgb[:N_rgb, :]
         disp = disp[:N_rgb] 
         acc = acc[:N_rgb]
@@ -328,7 +233,7 @@ def train(args):
             img_loss0 = img2mse(extras['rgb0'], target_s)
             loss = loss + img_loss0
         
-        optimizer.step(loss) # update weight
+        optimizer.step(loss) # update weight via back propagation
 
         # update learning rate
         decay_rate = 0.1
@@ -366,81 +271,6 @@ def train(args):
         global_step += 1
 
         sys.stdout.flush()
-
-# render function to get rgb_map, depth_map ...
-def render(H, W, focal, chunk=1024*32, rays=None, c2w=None, ndc=True,
-                  near=0., far=1., c2w_staticcam=None, depths=None,
-                  **kwargs):
-    if c2w is not None:
-        # render full image
-        rays_o, rays_d = get_rays(H, W, focal, c2w)
-    else:
-        # use provided ray batch
-        rays_o, rays_d = rays
-
-    viewdirs = rays_d
-    if c2w_staticcam is not None:
-        # visualize effect of viewdirs
-        rays_o, rays_d = get_rays(H, W, focal, c2w_staticcam)
-    viewdirs = viewdirs / jt.norm(viewdirs, dim=-1, keepdim=True)
-    viewdirs = jt.reshape(viewdirs, [-1,3]).float()
-
-    sh = rays_d.shape
-
-    # create ray batch
-    rays_o = jt.reshape(rays_o, [-1,3]).float()
-    rays_d = jt.reshape(rays_d, [-1,3]).float()
-
-    near, far = near * jt.ones_like(rays_d[...,:1]), far * jt.ones_like(rays_d[...,:1])
-    rays = jt.concat([rays_o, rays_d, near, far], -1) # B x 8
-    if depths is not None:
-        rays = jt.concat([rays, depths.reshape(-1,1)], -1)
-    rays = jt.concat([rays, viewdirs], -1)
-
-    # render and reshape
-    all_ret = batchify_rays(rays, chunk, **kwargs)
-    for k in all_ret:
-        k_sh = list(sh[:-1]) + list(all_ret[k].shape[1:])
-        all_ret[k] = jt.reshape(all_ret[k], k_sh)
-
-    k_extract = ['rgb_map', 'disp_map', 'acc_map', 'depth_map']
-    ret_list = [all_ret[k] for k in k_extract]
-    ret_dict = {k : all_ret[k] for k in all_ret if k not in k_extract}
-    return ret_list + [ret_dict]
-
-# render a sequence of images given by specific render poses (a render path)
-def render_path(render_poses, hwf, chunk, render_kwargs, gt_imgs=None, savedir=None):
-    H, W, focal = hwf
-    rgbs = []
-    disps = []
-    accs = []
-    
-    for i, c2w in enumerate(tqdm(render_poses)):
-        rgb, disp, acc, depth, extras = render(H, W, focal, chunk=chunk, c2w=c2w[:3,:4], **render_kwargs)
-        rgbs.append(rgb.numpy())
-        disps.append(disp.numpy())
-        accs.append(acc.numpy())
-        
-        if savedir is not None:
-            rgb8 = to8b(rgb.numpy())
-            rgb8[np.isnan(rgb8)] = 0
-            filename = os.path.join(savedir, '{:03d}.png'.format(i))
-            imageio.imwrite(filename, rgb8) # save rgb images
-            depth = depth.numpy()
-            depth = (np.clip(depth / 5, 0, 1) * 255.0).astype(np.uint8)
-            imageio.imwrite(os.path.join(savedir, '{:03d}_depth.png'.format(i)), depth) # save depth images
-        
-        # to save memory
-        del rgb
-        del disp
-        del acc
-        del extras
-        del depth
-
-    rgbs = np.stack(rgbs, 0)
-    disps = np.stack(disps, 0)
-
-    return rgbs, disps
 
 
 if __name__=='__main__':
